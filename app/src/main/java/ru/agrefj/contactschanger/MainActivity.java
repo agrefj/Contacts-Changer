23179fcd95337dad092745aa3454d512815e3319
Armen Davtyan <Armen Davtyan>
2016-03-28 03:15:26 +0300

    V1.1 Release

--- app/src/main/java/ru/agrefj/contactschanger/MainActivity.java
+++ app/src/main/java/ru/agrefj/contactschanger/MainActivity.java
@@ -22,7 +22,9 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -39,8 +41,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText et_digits;
     EditText et_newDigits;
 
-    Integer newNumber = 11111;
-    ProgressBar progressBar;
+    LinearLayout ll_progress;
+    TextView tv_done;
+    TextView tv_from;
+
+    Integer done = 0;
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -59,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         et_digits = (EditText) findViewById(R.id.et_digits);
         et_newDigits = (EditText) findViewById(R.id.et_newDigits);
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.READ_CONTACTS},
@@ -71,6 +77,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        ll_progress = (LinearLayout)findViewById(R.id.ll_progress);
+        ll_progress.setVisibility(View.INVISIBLE);
+
+
+        tv_from = (TextView)findViewById(R.id.tv_from);
     }
 
 
@@ -90,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void buttonReadyCheck() throws RemoteException, OperationApplicationException {
-        progressBar.setVisibility(View.VISIBLE);
+
 
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -98,105 +110,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
 
-        new CountDownTimer(20000, 100) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            public void onFinish() {
-                // When timer is finished
-                // Execute your code here
-                progressBar.setVisibility(View.INVISIBLE);
-            }
-        }.start();
 
 
         if (et_newDigits.getText().toString().length() > 0 && et_digits.getText().toString().length() > 0) {
 
-            new CountDownTimer(500, 100) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-
-                }
-
-                public void onFinish() {
-                   ChangeContactsActivity();
-
-
-                }
-            }.start();
+            ChangeContactsActivity();
 
         } else {
             Toast.makeText(MainActivity.this, this.getText(R.string.caution), Toast.LENGTH_SHORT).show();
-            progressBar.setVisibility(View.INVISIBLE);
+
         }
     }
-
-//    public void LoadContactsActivity() throws RemoteException, OperationApplicationException {
-//
-//        String digits = et_digits.getText().toString();
-//
-//        int length = digits.length();
-//
-//        String newDigits = et_newDigits.getText().toString();
-//
-//        ContentResolver cr = getContentResolver();
-//        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-//        if (cursor.getCount() > 0) {
-//            while (cursor.moveToNext()) {
-//
-//                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-//                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-//                Integer count = Integer.parseInt(cursor.getString(
-//                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
-//
-//
-//                if (Integer.parseInt(cursor.getString(
-//                        cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-//                    Cursor pCur = cr.query(
-//                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-//                            null,
-//                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-//                            new String[]{id}, null);
-//
-//                    String contactID = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-//                    while (pCur.moveToNext()) {
-//                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-//
-//
-//
-//                        phoneNo = phoneNo.replace(" ", "").replace("),", "").replace("(", "").replace("-", "").replace("—", "");
-//
-//                        if (phoneNo.length() > digits.length()) {
-//
-//                            String firstN = phoneNo.substring(0, length);
-//                            if (firstN.equals(digits)) {
-//                                ;
-//                                String changedStr = phoneNo.substring(length);
-//                                changedStr = newDigits + changedStr;
-//
-//                                updateContact(name, phoneNo, contactID, changedStr);
-//
-//
-//                            }
-//                        } else {
-//
-//                        }
-//
-//                    }
-//                    pCur.close();
-//
-//                }
-//            }
-//
-//        }
-//
-//    }
-//
-//
-
-
 
 
 
@@ -219,26 +143,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Retrieve all contacts
         Cursor cursorAll = contentResolver.query(uri,projection,selection,selectionArgs,null);
+        int count = cursorAll.getCount();
+
+        tv_from.setText(String.valueOf(count));
+        ll_progress.setVisibility(View.VISIBLE);
 
         if(cursorAll !=null){
-            cursorAll.moveToFirst();
+
 
             while (cursorAll.moveToNext())
             {
                 String contactID = cursorAll.getString(cursorAll.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+                String rawID = cursorAll.getString(cursorAll.getColumnIndex(ContactsContract.Contacts._ID));
 
-                Cursor phoneNumCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{contactID}, null);
+                Cursor phoneNumCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.Contacts._ID + " = ?", new String[]{rawID}, null);
 
-                phoneNumCursor.moveToFirst();
-                Integer count = phoneNumCursor.getCount();
-                Log.e("count", count.toString());
-
-               while (phoneNumCursor.moveToNext()){
+                while (phoneNumCursor.moveToNext()){
                     String phoneNumber = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DATA));
                     String numI = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
 
-//                    updateContact(contactID,phoneNumber,numI);
-                    Log.e("INI","phNum:" + phoneNumber +" . numId:" +numI + "   .ID is : "+contactID);
+                    updateContact(rawID,phoneNumber);
+
 
                 }
 
@@ -247,127 +172,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         }
-//        while (cursorAll.moveToNext()) {
-//            String contactID = cursorAll.getString(cursorAll.getColumnIndex(ContactsContract.Data.CONTACT_ID));
-//            Log.e("cid", contactID);
-//
-//
-//            Cursor phoneNumCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{contactID}, null);
-//
-//            phoneNumCursor.moveToFirst();
-//
-//            while (phoneNumCursor.moveToNext()) {
-//                String phoneNumber = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-//                String numI = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
-//
-//                updateContact(contactID,phoneNumber,numI);
-//                Log.e("phones",phoneNumber);
-//                Log.e("phI",numI);
-//            }
-//
-//            phoneNumCursor.close();
-//
-////
-//
-////            Integer count = phoneNumCursor.getCount();
-////            Log.e("count",count.toString());
-////            while (phoneNumCursor.moveToNext()){
-////                String phNum = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-////                String numI = phoneNumCursor.getString(phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
-////
-////                //Calling a ContentProviderOperation newUpdate;
-////
-////
-////                Log.e("phN",phNum);
-////                Log.e("num",numI);
-////
-////
-////
-////
-////
-////            }
-//        }
-
-//        while (cursor.moveToNext()){
-//
-//
-//
-//            String contactID = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-//            String selectionWhere = ContactsContract.Data.CONTACT_ID + " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
-//            String[] numberParams = new String[]{contactID, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE};
-//
-//
-//            String newPhone = "77777";
-//
-//            ArrayList<ContentProviderOperation> contentProviderOperations = new ArrayList<ContentProviderOperation>();
-//
-//
-//            ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
-//            ContentProviderOperation.Builder builder = ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI);
-//
-//            builder = ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI);
-//            builder.withSelection(ContactsContract.Data.CONTACT_ID + "=?" + " AND " + ContactsContract.Data.MIMETYPE + "=?"+ " AND " + ContactsContract.CommonDataKinds.Organization.TYPE + "=?", new String[]{String.valueOf(contactID), ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, String.valueOf(ContactsContract.CommonDataKinds.Phone.TYPE_HOME)});
-//            builder.withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, newPhone);
-//            ops.add(builder.build());
 
 
-
-//            contentProviderOperations.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
-//                    .withSelection(selectionWhere,numberParams)
-//                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER,newPhone)
-//                    .build()
-//            );
-
-//            try {
-//                contentResolver.applyBatch(ContactsContract.AUTHORITY, contentProviderOperations);
-//            } catch (RemoteException e) {
-//                e.printStackTrace();
-//            } catch (OperationApplicationException e) {
-//                e.printStackTrace();
-//            }
-
-            // Update
-//            try
-//            {
-//                getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
-//            }
-//            catch (Exception e)
-//            {
-//                e.printStackTrace();
-//            }
-
-
-
-
-//        }
 
     }
 
 
-    public void updateContact(String contactId, String number, String numberId) {
+    public void updateContact(String rawID,String phoneNumber) {
+        phoneNumber = phoneNumber.replace(" ", "").replace("),", "").replace("(", "").replace("-", "").replace("—", "");
+        Log.e("num",phoneNumber);
+        String digits = et_digits.getText().toString();
+        int length = digits.length();
+        String newDigits = et_newDigits.getText().toString();
 
-        String newNum = newNumber.toString();
-        newNumber++;
-        try {
-            ContentResolver contentResolver = this.getContentResolver();
+        done++;
+        String doneNums = String.valueOf(done);
+        tv_done = (TextView)findViewById(R.id.tv_done);
+        tv_done.setText(doneNums);
 
 
 
-            String where = ContactsContract.CommonDataKinds.Phone._ID+ " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
-            String[] numberParams = new String[]{numberId, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE};
-            ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        if(phoneNumber.length()>digits.length()){
+            String firstDig = phoneNumber.substring(0,length);
 
-            ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
-                    .withSelection(where, numberParams)
-                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, newNum)
-                    .build());
+            if (firstDig.equals(digits)){
+                String newNum = phoneNumber.substring(length);
+                newNum = newDigits + newNum;
 
-            contentResolver.applyBatch(ContactsContract.AUTHORITY, ops);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+                try {
+                    ContentResolver contentResolver = this.getContentResolver();
+
+
+
+                    String where = ContactsContract.Contacts._ID+ " = ? AND " + ContactsContract.Data.MIMETYPE + " = ?";
+                    String[] numberParams = new String[]{rawID, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE};
+                    ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+
+                    ops.add(ContentProviderOperation.newUpdate(ContactsContract.Data.CONTENT_URI)
+                            .withSelection(where, numberParams)
+                            .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, newNum)
+                            .build());
+
+                    contentResolver.applyBatch(ContactsContract.AUTHORITY, ops);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+        }
+
+
+
+
+
+
+
+
 
         }
+
+
+
+
+
+
 
     }
 
